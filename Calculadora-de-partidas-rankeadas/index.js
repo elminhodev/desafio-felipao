c33be93bb08435c70840b71267a0741308cb0afb
elminhodev <miinho.sk8@gmail.com>
2024-11-03 13:50:45 -0300

    Desafio 2

---
+++ Calculadora-de-partidas-rankeadas/index.js
@@ -0,0 +1,32 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+// Defina suas vitórias e derrotas aqui
+let vitorias = 30;  // Substitua pelo número de vitórias desejado
+let derrotas = 15;  // Substitua pelo número de derrotas desejado
+
+// Chama a função e armazena o resultado
+const resultado = calcularNivel(vitorias, derrotas);
+
+// Exibe a mensagem final
+console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
